3e4e7df9f27dbd4302c6eab612dd2090c9180e69
Leilson <leilsonf@gmail.com>
2021-09-29 21:36:03 -0300

    base de dados

---
+++ almoxarifado.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 19-Set-2021 às 02:43
+-- Versão do servidor: 5.7.26
+-- versão do PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `almoxarifado`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `departamentos`
+--
+
+DROP TABLE IF EXISTS `departamentos`;
+CREATE TABLE IF NOT EXISTS `departamentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fornecedores`
+--
+
+DROP TABLE IF EXISTS `fornecedores`;
+CREATE TABLE IF NOT EXISTS `fornecedores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `cnpj` varchar(50) DEFAULT NULL,
+  `uf` varchar(3) DEFAULT NULL,
+  `tipo_estabelecimento` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `grupos`
+--
+
+DROP TABLE IF EXISTS `grupos`;
+CREATE TABLE IF NOT EXISTS `grupos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `movimentacoes`
+--
+
+DROP TABLE IF EXISTS `movimentacoes`;
+CREATE TABLE IF NOT EXISTS `movimentacoes` (
+  `entrada` int(11) DEFAULT NULL,
+  `saida` int(11) DEFAULT NULL,
+  `devolucao` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `movimentacoes`
+--
+
+INSERT INTO `movimentacoes` (`entrada`, `saida`, `devolucao`) VALUES
+(0, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `movimentacoes_estoque`
+--
+
+DROP TABLE IF EXISTS `movimentacoes_estoque`;
+CREATE TABLE IF NOT EXISTS `movimentacoes_estoque` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produto` int(11) DEFAULT NULL,
+  `quant_mov` decimal(10,0) DEFAULT NULL,
+  `motivo` varchar(500) DEFAULT NULL,
+  `data_mov` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `movimentacao` int(11) NOT NULL,
+  `departamento` int(11) DEFAULT NULL,
+  `responsavel` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id_fk_mov` (`responsavel`) USING BTREE,
+  KEY `produto_id_fk_mov` (`produto`) USING BTREE,
+  KEY `departamento_id_fk_mov` (`departamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(100) DEFAULT NULL,
+  `quantidade` varchar(60) DEFAULT NULL,
+  `unidade` varchar(60) DEFAULT NULL,
+  `fornecedor` int(11) NOT NULL,
+  `grupo` int(11) NOT NULL,
+  `data_cadastro` datetime DEFAULT CURRENT_TIMESTAMP,
+  `data_alteracao` datetime DEFAULT NULL,
+  `responsavel` varchar(80) DEFAULT NULL,
+  `valor_unidade` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `fornecedor_id_fk_prod` (`fornecedor`) USING BTREE,
+  KEY `grupo_id_fk_prod` (`grupo`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(80) DEFAULT NULL,
+  `senha` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `senha`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `movimentacoes_estoque`
+--
+ALTER TABLE `movimentacoes_estoque`
+  ADD CONSTRAINT `departamento_id_fk_mov` FOREIGN KEY (`departamento`) REFERENCES `departamentos` (`id`),
+  ADD CONSTRAINT `produto_id_fk_mov` FOREIGN KEY (`produto`) REFERENCES `produtos` (`codigo`),
+  ADD CONSTRAINT `usuario_id_fk_mov` FOREIGN KEY (`responsavel`) REFERENCES `usuarios` (`id`);
+
+--
+-- Limitadores para a tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `fornecedor_id_fk_prod` FOREIGN KEY (`fornecedor`) REFERENCES `fornecedores` (`id`),
+  ADD CONSTRAINT `grupo_id_fk_prod` FOREIGN KEY (`grupo`) REFERENCES `grupos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
