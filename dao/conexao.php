fadf4fa91e0dc219f267ce2e2221eddf58bccbb1
Leilson <leilsonf@gmail.com>
2021-09-30 22:23:28 -0300

    alterei o nome do banco

--- dao/conexao.php
+++ dao/conexao.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $usuario = "root";
 $senha = "";
-$banco="estoque1";
+$banco="almoxarifado";
 
 $conexao= mysqli_connect($host,$usuario,$senha,$banco) or die("Não foi possivel conectar ao banco");
 mysqli_select_db($conexao,$banco) or die("Não foi possivel encontrar esse banco");
