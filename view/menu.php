407e0456346faf3f9d15a1d972143a4ec3a7baf2
Leilson <leilsonf@gmail.com>
2021-10-19 21:20:08 -0300

    Retirado o menu Departamentos

--- view/menu.php
+++ view/menu.php
@@ -7,16 +7,8 @@
             <i class="fas fa-fw fa-university"></i>
             <span>Painel Principal</span>
         </a>
-    </li>
+    </li>	
 	
-	<li class="nav-item">
-        <a class="nav-link <?php if ($pagina == "painel") {
-                                echo "active";
-                            } ?>" href="lista_departamentos.php">
-            <i class="fa fa-sitemap"></i>
-            <span>Departamentos</span>
-        </a>	
-	</li>
 	<li class="nav-item">
         <a class="nav-link <?php if ($pagina == "painel") {
                                 echo "active";
