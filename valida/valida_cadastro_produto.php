c5a1586d95e7585f12fcd216be80fb936fa74567
Leilson <leilsonf@gmail.com>
2021-10-27 19:00:39 -0300

    Informar se o item já existe no banco

--- valida/valida_cadastro_produto.php
+++ valida/valida_cadastro_produto.php
@@ -3,34 +3,42 @@ ob_start();
 session_start();
 require "../dao/conexao.php";
 
-if (isset($_POST['cadastrar'])) {
+	if (isset($_POST['cadastrar'])) {
 
-    $descricao = ($_POST['descricao']);
-    $quantidade = ($_POST['quantidade']);
-    $unidade = ($_POST['unidade']);    
-    $select_fornecedor = ($_POST['select_fornecedor']);
-	$select_grupo = ($_POST['select_grupo']);
-    $valor_unidade = ($_POST['valor_unidade']);
-    $responsavel = $_SESSION['usuario'];
+			$descricao = ($_POST['descricao']);
+			$quantidade = ($_POST['quantidade']);
+			$unidade = ($_POST['unidade']);    
+			$select_fornecedor = ($_POST['select_fornecedor']);
+			$select_grupo = ($_POST['select_grupo']);
+			$valor_unidade = ($_POST['valor_unidade']);
+			$responsavel = $_SESSION['usuario'];
 
-    //inserir no banco.
-    $sql = "INSERT INTO produtos (descricao,quantidade,unidade,fornecedor,grupo,valor_unidade,responsavel)
-          VALUE ('$descricao','$quantidade','$unidade','$select_fornecedor','$select_grupo','$valor_unidade','$responsavel')";
+			$sql = "SELECT 0 FROM produtos where descricao='$descricao'";
+			$resultado1 = mysqli_query($conexao,$sql);
+				if(mysqli_affected_rows($conexao) == 0){	
+					//inserir no banco.
+					$sql = "INSERT INTO produtos (descricao,quantidade,unidade,fornecedor,grupo,valor_unidade,responsavel)
+						  VALUE ('$descricao','$quantidade','$unidade','$select_fornecedor','$select_grupo','$valor_unidade','$responsavel')";
+					//Incluir
+					$resultado = mysqli_query($conexao, $sql);
 
-    //Incluir
-    $resultado = mysqli_query($conexao, $sql);
+						if (!isset($resultado)){							
+							$_SESSION['prod_nao_cadastrado'] = true;
+							header('Location:../view/lista_produtos.php');
 
-    if (!isset($resultado)){
-        echo "Falha ao inserir dados!";
-        $_SESSION['prod_nao_cadastrado'] = true;
-        header('Location:../view/lista_produtos.php');
+						} else {
+							$_SESSION['prod_cadastrado'] = true;
+							header('Location:../view/lista_produtos.php');
+							exit();							
+						}
+				}else{
+					$_SESSION['prod_ja_existe'] = true;
+					header('Location:../view/lista_produtos.php');
+				}
+		}else {
+			echo "<script> alert('Não foi possível fazer o cadastro');</script>";
+	}
 
-    } else {
-        $_SESSION['prod_cadastrado'] = true;
-        header('Location:../view/lista_produtos.php');
-        exit();
-        
-    }
-} else {
-    echo "<script> alert('Não foi possível fazer o cadastro');</script>";
-}
+
+ 
+	
