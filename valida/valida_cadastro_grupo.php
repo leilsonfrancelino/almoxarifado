d8c344cab355e970cff7c91760a4a4591f5e676d
Leilson <leilsonf@gmail.com>
2021-10-27 19:15:29 -0300

    Informar se o item já existe no banco

--- valida/valida_cadastro_grupo.php
+++ valida/valida_cadastro_grupo.php
@@ -3,28 +3,33 @@ ob_start();
 session_start();
 require "../dao/conexao.php";
 
-if (isset($_POST['cadastrar'])) {
+	if (isset($_POST['cadastrar'])) {
 
-    $nome_grupo = ($_POST['nome_grupo']);
-       
+		$nome_grupo = ($_POST['nome_grupo']);
+		   
+		$sql = "SELECT 0 FROM grupos where nome='$nome_grupo'";
+		$resultado1 = mysqli_query($conexao,$sql);
+				if(mysqli_affected_rows($conexao) == 0){
+					//inserir no banco.
+					$sql = "INSERT INTO grupos (nome)
+						  VALUE ('$nome_grupo')";
 
-    //inserir no banco.
-    $sql = "INSERT INTO grupos (nome)
-          VALUE ('$nome_grupo')";
+					//Incluir
+					$resultado = mysqli_query($conexao, $sql);
 
-    //Incluir
-    $resultado = mysqli_query($conexao, $sql);
+					if (!isset($resultado)){
+						$_SESSION['grupo_nao_cadastrado'] = true;
+						header('Location:../view/lista_grupos.php');
 
-    if (!isset($resultado)){
-        $_SESSION['grupo_nao_cadastrado'] = true;
-        header('Location:../view/lista_grupos.php');
-
-    } else {
-        $_SESSION['grupo_cadastrado'] = true;
-        header('Location:../view/lista_grupos.php');
-        exit();
-        
-    }
-} else {
-    echo "<script> alert('Não foi possível fazer o cadastro');</script>";
-}
+					} else {
+						$_SESSION['grupo_cadastrado'] = true;
+						header('Location:../view/lista_grupos.php');
+						exit();			
+			        }
+				}else{
+					$_SESSION['grupo_ja_existe'] = true;
+					header('Location:../view/lista_grupos.php');
+				}
+	} else {
+		echo "<script> alert('Não foi possível fazer o cadastro');</script>";
+	}
