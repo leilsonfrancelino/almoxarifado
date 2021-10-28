3a9878c14b1e6d03918dd97a94a2ee16936560f3
Leilson <leilsonf@gmail.com>
2021-10-28 19:58:23 -0300

    Adicionado mensagem quando houver relacionamento tabela chave estrangeira

--- view/confirmar_exclusao_fornecedor.php
+++ view/confirmar_exclusao_fornecedor.php
@@ -7,15 +7,24 @@ require "../dao/conexao.php";
 $id_fornec = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 	if(!empty($id_fornec)) {
 		
-		$sql = "DELETE FROM fornecedores where id='$id_fornec'";
-		$resultado = mysqli_query($conexao,$sql);
-			if(mysqli_affected_rows($conexao) == 0){
-				$_SESSION['fornecedor_nao_deletado'] = true;
-				header('Location:../view/lista_fornecedores.php');
-			}else{
-				$_SESSION['fornecedor_deletado'] = true;
-				header('Location:../view/lista_fornecedores.php');	
-			}
+		$sql =	"SELECT f.id FROM fornecedores as f INNER JOIN produtos as p ON f.id = p.grupo WHERE codigo=$id_fornec";
+		$resultado1 = mysqli_query($conexao,$sql);
+		
+		if(mysqli_affected_rows($conexao) == 0) { 
+			$sql = "DELETE FROM fornecedores where id='$id_fornec'";
+			$resultado = mysqli_query($conexao,$sql);
+				if(mysqli_affected_rows($conexao) == 0){
+						$_SESSION['fornecedor_nao_deletado'] = true;
+						header('Location:../view/lista_fornecedores.php');
+					}else{
+						$_SESSION['fornecedor_deletado'] = true;
+						header('Location:../view/lista_fornecedores.php');	
+					}
+				}else{
+					$_SESSION['chave_estrangeira_fornecedor'] = true;
+					header('Location:../view/lista_fornecedores.php');
+			
+		}	
 	}else{
 		$_SESSION['selecione_fornecedor'] = true;
 		header('Location:../view/lista_fornecedores.php');	
