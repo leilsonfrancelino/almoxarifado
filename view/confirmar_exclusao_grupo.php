7d27ec564b774a6d70d6b4ab6e57ad9d69fd396c
Leilson <leilsonf@gmail.com>
2021-10-28 18:40:26 -0300

    Adicionado mensagem quando houver relacionamento tabela chave estrangeira

--- view/confirmar_exclusao_grupo.php
+++ view/confirmar_exclusao_grupo.php
@@ -4,25 +4,29 @@ session_start();
 require "../dao/conexao.php";
 
 
-$id_grupo = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$id_grupo = filter_input(INPUT_POST, 'id_grupo', FILTER_SANITIZE_NUMBER_INT);
 	if(!empty($id_grupo)) {
 		
-		$sql = "DELETE FROM grupos where id='$id_grupo'";
+		$sql =	"SELECT g.id FROM grupos as g INNER JOIN produtos as p ON g.id = p.grupo WHERE codigo=$id_grupo";
 		$resultado = mysqli_query($conexao,$sql);
-			if(mysqli_affected_rows($conexao) == 0){
-				$_SESSION['grupo_nao_deletado'] = true;
-				header('Location:../view/lista_grupos.php');
-			}else{
-				$_SESSION['grupo_deletado'] = true;
-				header('Location:../view/lista_grupos.php');	
-			}
+			
+		if(mysqli_affected_rows($conexao) == 0) {  
+            $_SESSION['chave_estrangeira_grupo'] = true;
+			header('Location:../view/lista_grupos.php');
+        }else{
+			$sql = "DELETE FROM grupos where id='$id_grupo'";
+			$resultado = mysqli_query($conexao,$sql);
+				if(mysqli_affected_rows($conexao) == 0){
+					$_SESSION['grupo_nao_deletado'] = true;
+					header('Location:../view/lista_grupos.php');
+				}else{
+					$_SESSION['grupo_deletado'] = true;
+					header('Location:../view/lista_grupos.php');	
+				}
+		}	
 	}else{
 		$_SESSION['selecione_grupo'] = true;
 		header('Location:../view/lista_grupos.php');	
 	}
+
 ?>
-
-
-
-
-
